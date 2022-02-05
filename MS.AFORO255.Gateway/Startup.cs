93a505d8130c12cd333b32db987831d662015f41
jeersson <jmaradiaga2520@gmail.com>
2022-02-04 19:48:09 -0600

    agregando Cors

--- MS.AFORO255.Gateway/Startup.cs
+++ MS.AFORO255.Gateway/Startup.cs
@@ -2,17 +2,10 @@ using Aforo255.Cross.Token.Src;
 using Aforo255.Cross.Tracing.Src;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MS.AFORO255.Gateway
 {
@@ -24,10 +17,22 @@ namespace MS.AFORO255.Gateway
         }
 
         public IConfiguration Configuration { get; }
+        readonly string clientPolicy = "_clientPolicy";
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            /*Start - Cors*/
+            services.AddCors(o => o.AddPolicy(clientPolicy, builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+
+            }));
+            services.AddRouting(r => r.SuppressCheckForUnhandledSecurityMetadata = true);
+            /*End - Cors*/
+
             services.AddJwtCustomized("MY-KEY-JWT");
             services.AddOcelot();
 
@@ -40,6 +45,15 @@ namespace MS.AFORO255.Gateway
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            /*Start - Cors*/
+            app.UseCors(clientPolicy);
+            app.Use((context, next) =>
+            {
+                context.Items["__CorsMiddlewareInvoked"] = true;
+                return next();
+            });
+            /*End - Cors*/
+
             app.UseOcelot().Wait();
         }
     }
